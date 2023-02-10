b1cd4e17f5b1df2603c8f30ea34027feb094aa70
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-10 15:41:25 +0700

    th3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Login</h1>
+      <form>
+        <p className={styles.fieldTitle}>Nhập email:</p>
+        <input
+          name="email"
+          type="text"
+          className={styles.input}
+        />
+        <p className={styles.fieldTitle}>Nhập password:</p>
+        <input
+          name="password"
+          type="password"
+          className={styles.input}
+        />
+        <br />
+        <label>
+          <input
+            className={styles.checkbox}
+            name="isRead"
+            type="checkbox" /> I read and accept the privacy policy:
+        </label>
+
+        <button className={styles.btnSubmit}>Submit nè</button>
+      </form>
     </div>
   );
 }
-
-export default App;
